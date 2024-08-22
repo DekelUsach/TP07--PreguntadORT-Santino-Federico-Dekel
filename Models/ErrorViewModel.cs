542c00d8a469513c05ad27eeca98422a32c1ff9c
DEKEL USACH <48386383@ort>
2024-08-22 14:04:18 -0300

    Creation of MVC project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP07__PreguntadORT_Santino_Federico_Dekel.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
