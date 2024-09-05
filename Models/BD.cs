2df40d29aa0631345557b8fcaeb50e18031c7d51
FEDERICO SEBASTISAN AZUBEL FINGERHUT <48677630@ort>
2024-09-05 14:31:03 -0300

    Finished view Index, at least I think so

--- Models/BD.cs
+++ Models/BD.cs
@@ -4,7 +4,7 @@ using Dapper;
 public static class BD
 {
     /* ATRIBUTOS */
-    private static string _connectionString = @"Server=localhost;Database=TriviaDB;Trusted_Connection=True;";
+    private static string _connectionString = @"Server=A-PHZ2-CIDI-11;Database=TriviaDB;Trusted_Connection=True;";
     public static List<Categorias> ListaCategorias = new List<Categorias>();
     public static List<Dificultades> ListaDificultades = new List<Dificultades>();
 
